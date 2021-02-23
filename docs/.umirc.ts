74abe39046b604bcd2b250e47fa95e9b19f6819e
junping.hjp <junping.hjp@dtwave-inc.com>
2021-02-23 08:18:31 +0800

    docs

---
+++ docs/.umirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'cloudbase-context',
+  mode: 'site',
+  publicPath: './',
+  // more config: https://d.umijs.org/config
+});
---
+++ docs/docs/guide/base.md
@@ -0,0 +1,12 @@
+---
+nav:
+  title: 基础
+  order: 1
+  toc: 'menu'
+---
+
+# 基础
+111
+
+## 什么是cloudbase-context
+111
