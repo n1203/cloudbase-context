db5907a24a181ff4d408104c1b42a6e7a689f3e8
junping.hjp <junping.hjp@dtwave-inc.com>
2021-02-23 08:40:19 +0800

    docs

--- docs/.umirc.ts
+++ docs/.umirc.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   title: 'cloudbase-context',
   mode: 'site',
   publicPath: './',
+  logo: `http://cloudbase-context.github.gold/logo.svg`,
   // more config: https://d.umijs.org/config
 });
